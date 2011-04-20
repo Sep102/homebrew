6139eb4fb346c7063ef50073fd43e0396917e76b
Michael Lapinsky <sep102@gmail.com>
2011-04-20 01:34:49 -0700

    Added open content downloading to openttd
    Automatically downloads OpenGFX, OpenMSX and OpenSFX as part
    of the install process so that the bundle will work "out of the box"

--- Library/Formula/openttd.rb
+++ Library/Formula/openttd.rb
@@ -12,13 +12,32 @@ class Openttd < Formula
   def install
     system "./configure", "--prefix-dir=#{prefix}"
     system "make bundle"
+    
+    ohai 'Downloading freeware data packs (OpenGFX, OpenMSX, OpenSFX).'
+    cd 'bundle/OpenTTD.app/Contents/Resources' do
+      cd 'data' do
+        curl 'http://bundles.openttdcoop.org/opengfx/releases/0.3.3/opengfx-0.3.3.zip',
+             'http://bundles.openttdcoop.org/opensfx/releases/0.2.3/opensfx-0.2.3.zip',
+             '-OO'
+
+        system 'for zip in open*.zip; do unzip $zip; rm $zip; done'
+      end
+      cd 'gm' do
+        curl 'http://bundles.openttdcoop.org/openmsx/releases/0.3.1/openmsx-0.3.1.zip',
+             '-O'
+
+        system 'for zip in open*.zip; do unzip $zip; rm $zip; done'
+      end
+    end
+
     prefix.install 'bundle/OpenTTD.app'
   end
 
   def caveats; <<-EOS.undent
-    Note: This does not install any art assets. You will be prompted
-    on first run to download an asset set.
-    Having an original TTD CD will help.
+      OpenTTD.app installed to: #{prefix}
+      If you have access to the sound and graphics files from the original 
+      Transport Tycoon Deluxe, you can install them by following the 
+      instructions in section 4.2 of the OpenTTD readme.
     EOS
   end
 end
